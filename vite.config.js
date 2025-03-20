9e52d12402be8de317a1e242e98f4fc3c2f80ca4
quangeagle <haoquang16122004@gmail.com>
2025-03-20 11:36:15 +0700

    7

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Đảm bảo file build ra thư mục "dist"
+  },
 })
