e152c9c8d5bba355ca0b966270730be6437e0c85
quangeagle <haoquang16122004@gmail.com>
2025-03-22 18:52:27 +0700

    quangeagle17

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Đảm bảo đúng đường dẫn khi deploy lên Vercel
+  base: '', // Đảm bảo đường dẫn tuyệt đối
   build: {
     outDir: 'dist',
   },
